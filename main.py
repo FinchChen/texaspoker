d15db8d9a7f09915ecb90dd85f7f7f49503a4dca
Finch <finchyifanchen@gmail.com>
2018-03-02 03:00:35 +0000

    update

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ class main(object):
     
 
     while True:
-        
+
         dealer = newfunc.dealer(2)
         dealer.start()
         print 'Press enter to restart.'
         tmp = raw_input()
 
+    
     
